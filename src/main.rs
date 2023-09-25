56e7d26ad1f3982a702049c4bbc9c1b559e4624a
Brendan Nolan <bnolan23@gmail.com>
2023-09-25 07:32:58 +0100

    More Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::blockchain::BlockChain;
+use crate::{blockchain::BlockChain, p2p::AppBehaviour};
 use libp2p::{
     core::upgrade,
     futures::StreamExt,
@@ -64,34 +64,39 @@ async fn main() {
 
     loop {
         select! {
-            line = stdin.next_line() => {
-                let line = line.unwrap().unwrap();
-                match line.as_str() {
-                    "ls p" => p2p::print_peers(&swarm),
-                    cmd if cmd.starts_with("ls c") => p2p::print_chain(&swarm),
-                    cmd if cmd.starts_with("create b") => {
-                        if let Some(data) = cmd.strip_prefix("create b") {
-                            let new_block = swarm.behaviour_mut().blockchain.mine_block_return_mined_clone(data);
-                            p2p::send_block(new_block, &mut swarm);
-                        }
-                    }
-                    _ => error!("unknown command"),
-                }
-            }
-            _ = init_rcv.recv() => {
-                let peers = p2p::get_peers(&swarm);
-                swarm.behaviour_mut().blockchain.genesis();
-
-                info!("connected nodes: {}", peers.len());
-                if !peers.is_empty() {
-                    let last_peer = peers.last().expect("can get last peer");
-                    p2p::request_chain(&mut swarm, last_peer.clone());
-                }
-            }
+            line = stdin.next_line() => execute_user_command(&line.unwrap().unwrap(), &mut swarm),
+            _ = init_rcv.recv() => setup_initial_blockchain(&mut swarm),
             event = swarm.select_next_some() => {
                 info!("Unhandled Swarm Event: {:?}", event);
                 continue;
             },
         }
+    }
+}
+
+fn execute_user_command(line: &str, swarm: &mut Swarm<AppBehaviour>) {
+    match line {
+        "ls p" => p2p::print_peers(swarm),
+        cmd if cmd.starts_with("ls c") => p2p::print_chain(swarm),
+        cmd if cmd.starts_with("create b") => {
+            if let Some(data) = cmd.strip_prefix("create b") {
+                let new_block = swarm
+                    .behaviour_mut()
+                    .blockchain
+                    .mine_block_return_mined_clone(data);
+                p2p::send_block(new_block, swarm);
+            }
+        }
+        _ => error!("unknown command"),
+    }
+}
+
+fn setup_initial_blockchain(swarm: &mut Swarm<AppBehaviour>) {
+    let peers = p2p::get_peers(swarm);
+    swarm.behaviour_mut().blockchain.genesis();
+    info!("connected nodes: {}", peers.len());
+    if !peers.is_empty() {
+        let last_peer = peers.last().expect("can get last peer");
+        p2p::request_chain(swarm, last_peer.clone());
     }
 }
