e9326d5018515656e7ba81857e628938bd1796cb
Brendan Nolan <bnolan23@gmail.com>
2023-10-16 07:34:48 +0100

    IMprove Logging In Retry Loop

--- src/retry.rs
+++ src/retry.rs
@@ -1,3 +1,4 @@
+use log::info;
 use std::time::Duration;
 use tokio::{sync::mpsc, time};
 
@@ -6,12 +7,16 @@ pub async fn run_retry_loop(
     tx_retry_request: mpsc::UnboundedSender<()>,
     retry_frequency: Duration,
 ) {
+    info!("----------Trying----------");
+    let _ = tx_retry_request.send(());
     loop {
         tokio::select! {
             _ = time::sleep(retry_frequency) => {
+                info!("----------Retrying----------");
                 let _ = tx_retry_request.send(());
             },
             _ = rx_stop_request.recv() => {
+                info!("----------Succeeded, No Longer Retrying----------");
                 break;
             }
         }
